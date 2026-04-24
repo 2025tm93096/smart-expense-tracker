e59611e06496908254deb38e0292cfd25558d508
Susheel <2025tm93096@wilp.bits-pilani.ac.in>
2026-04-24 06:25:59 +0100

    feat: custom wallet favicon, update tab title and manifest

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" }); // backend URL
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+});
 
 // Attach token to requests
 API.interceptors.request.use((req) => {
